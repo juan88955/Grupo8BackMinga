bdd6317df000fee6a7cb56d1aa8adf67301336cb
Allpez <alexlop1987@gmail.com>
2024-12-03 02:10:42 -0500

    modificacion controladore manga

--- controllers/mangas/read.js
+++ controllers/mangas/read.js
@@ -1,14 +1,15 @@
 import Manga from '../../models/Manga.js'
+import Chapter from '../../models/Chapter.js';
 
 let allMangas = async (req, res, next) => {
     try {
-        let {name} = req.query 
+        let { name } = req.query
         let query = {} //Enviamos un objeto vacio, Traer todas los Mangas
 
-        if (name){
-            query.name = {$regex: '^'+name, $options: 'i'}//Prevalidaciones
+        if (name) {
+            query.name = { $regex: '^' + name, $options: 'i' }//Prevalidaciones
         }
-        
+
         let manga = await Manga.find(query);
         return res.status(200).json({
             response: manga
@@ -22,13 +23,13 @@ let mangasByTitle = async (req, res, next) => {
     try {
         let titleQuery = req.params.title;
         let manga;
-        
+
         if (titleQuery) {
             manga = await Manga.find({ title: { $regex: `${titleQuery}`, $options: "i" } });
         } else {
             manga = await Manga.find();
         }
-        
+
         return res.status(200).json({
             response: manga
         });
@@ -36,7 +37,7 @@ let mangasByTitle = async (req, res, next) => {
         next(error);
     }
 };
-    
+
 const mangasById = async (req, res) => {
     try {
         let manga = await Manga.findById(req.params.id)
@@ -49,4 +50,18 @@ const mangasById = async (req, res) => {
     }
 }
 
-export { allMangas, mangasById, mangasByTitle }
+let mangaWithChapters = async (req, res, next) => {
+    try {
+        const mangaId = req.params.id;
+        const manga = await Manga.findById(mangaId);
+        const chapters = await Chapter.find({ manga_id: mangaId });
+
+        res.status(200).json({
+            response: { ...manga.toObject(), chapters }
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export { allMangas, mangasById, mangasByTitle, mangaWithChapters }
