6861106ab597c48262bbb98777f228edc9c48b59
kris0395 <khrispitbrown@gmail.com>
2024-12-04 23:09:25 -0500

    ajustes al schema create.js

--- schemas/users/create.js
+++ schemas/users/create.js
@@ -11,7 +11,7 @@ const userSchema = Joi.object({
     password: Joi.string()
         .required()
         .min(5)
-        .pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+        .pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]*$/)
         .messages({
             'string.min': 'Password must be at least 5 characters',
             'string.pattern.base': 'The password must include at least one letter, one number, and one special character.',
@@ -22,20 +22,6 @@ const userSchema = Joi.object({
         .uri()
         .messages({
             'string.uri': 'Photo URL is invalid'
-        }),
-    role: Joi.number()
-        .required()
-        .positive()
-        .precision(1)
-        .messages({
-            'number.base': 'The value must be a number',
-            'number.positive': 'The value must be positive',
-            'number.precision': 'The value can have up to only one number'
-        }),
-    online: Joi.boolean()
-        .optional()
-        .messages({
-            'boolean.base': 'The "online" field must be a boolean value (true or false).'
         })
 });
 
